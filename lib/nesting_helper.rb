293530d30e7ce7a87b1c4007b7a93e0516c4caba
Oliver Diamond <oliverdiamond@Olivers-MacBook-Pro-3.local>
2016-06-09 19:11:41 +0100

    Working: Recursive nesting

--- lib/nesting_helper.rb
+++ lib/nesting_helper.rb
@@ -1,13 +1,7 @@
 module NestingHelper
 
   def nest(*args)
-  args.count >= 1 ? erb(args.pop) { nest(args) } : erb(args.pop)
-
-    # erb(args[0]) do
-    #   erb(args[1]) do
-    #     erb(args[2])
-    #   end
-    # end
+   args.flatten!
+   args.last != :test ? erb(args.pop, layout: false) { nest(args) } : erb(args.last)
   end
-
 end
