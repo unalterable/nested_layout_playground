4d35deb3cbda24316860c1a36d75f69d48ef1d4b
Oliver Diamond <oliverdiamond@Olivers-MacBook-Pro-2.local>
2016-06-14 18:16:36 +0100

    Nesting All working - visit /test

--- app.rb
+++ app.rb
@@ -2,16 +2,19 @@ require 'sinatra/base'
 require_relative 'lib/nesting_helper'
 
 class App < Sinatra::Base
-  include NestingHelper
+  
 
   get '/' do
     erb(:test)
   end
 
   get '/test' do
-    nest([:test, :layout1, :layout1, :layout2, :layout3, :layout1, :layout2, :layout3])
+    NestingHelper.new.nest([:test, :layout1, :layout1, :layout2, :layout3, :layout1, :layout2, :layout3])
   end
-
+  
+  get '/test2' do
+    NestingHelper.new.nest([:test, :layout1])
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- lib/nesting_helper.rb
+++ lib/nesting_helper.rb
@@ -1,6 +1,34 @@
-module NestingHelper
+require 'erb'
+require 'sinatra/base'
 
-  def nest(args)
-   args.count >= 1 ? erb(args.pop) { nest(args) } : erb(args.last)
+class NestingHelper 
+
+  class LayoutRenderer
   end
+
+
+    def nest(args)
+   args.count >= 1 ? erb(args.pop){ nest(args) } : erb(args.last)
+  end
+
+  def erb(sym, &block)
+    erb = ERB.new(find(sym))
+    erb.def_method(LayoutRenderer, :render)
+    puts block.methods.sort
+    return LayoutRenderer.new.render{ block.call } if block_given?
+    erb.result
+  end
+
+  def find(sym, ext = ".erb", folder = "views")
+    filename = sym.to_s + ext
+    dir = File.join(Dir.pwd, folder, filename)
+    p '----------------------------'
+    p dir
+    p Dir.pwd
+    file = File.open(dir, 'r')
+    output_str = file.read
+    file.close
+    output_str
+  end
+
 end
