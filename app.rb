21c7c662ea7e15502cf5693c1ae810fe271013a4
Oliver Diamond <oliverdiamond@Olivers-MacBook-Pro-4.local>
2016-06-15 15:28:30 +0100

    Working and refactored

--- app.rb
+++ app.rb
@@ -8,13 +8,22 @@ class App < Sinatra::Base
     erb(:test)
   end
 
-  get '/test' do
+  get '/test1' do
     NestingHelper.new.nest([:test, :layout1, :layout1, :layout2, :layout3, :layout1, :layout2, :layout3])
   end
   
   get '/test2' do
     NestingHelper.new.nest([:test, :layout1])
   end
+  
+  get '/test3' do
+    NestingHelper.new.nest([:layout1, :layout2, :layout3, :layout2])
+  end
+  
+  get '/test4' do
+    NestingHelper.new.my_erb(:layout2)
+  end
+  
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- lib/nesting_helper.rb
+++ lib/nesting_helper.rb
@@ -2,33 +2,40 @@ require 'erb'
 require 'sinatra/base'
 
 class NestingHelper 
+  
+  
+  def nest(args)
+   args.count > 1 ? my_erb(args.pop){ nest(args) } : my_erb(args.last)
+  end
+  
+  def my_erb(sym, &block)
+    puts "================="
+    puts sym
+    block ||= Proc.new { "" }
+    erb = ERB.new( find_file(sym) )
+    unknown_crazy(erb, block)
+  end
+  
+  private
 
+  def find_file(sym, ext = ".erb", folder = "views")
+    dir = File.join(Dir.pwd, folder, sym.to_s + ext)
+    read_file(dir)
+  end
+  
+  def read_file(dir)
+    file = File.open(dir, 'r')
+    result = file.read
+    file.close
+    result
+  end
+  
   class LayoutRenderer
   end
-
-
-    def nest(args)
-   args.count >= 1 ? erb(args.pop){ nest(args) } : erb(args.last)
-  end
-
-  def erb(sym, &block)
-    erb = ERB.new(find(sym))
+  
+  def unknown_crazy(erb, block)
     erb.def_method(LayoutRenderer, :render)
-    puts block.methods.sort
-    return LayoutRenderer.new.render{ block.call } if block_given?
-    erb.result
-  end
-
-  def find(sym, ext = ".erb", folder = "views")
-    filename = sym.to_s + ext
-    dir = File.join(Dir.pwd, folder, filename)
-    p '----------------------------'
-    p dir
-    p Dir.pwd
-    file = File.open(dir, 'r')
-    output_str = file.read
-    file.close
-    output_str
+    LayoutRenderer.new.render{ block.call }
   end
 
 end
